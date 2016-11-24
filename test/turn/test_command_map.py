652b8eef3757440b2e7d9cf17e4bb764862510b4
unknown <aric.parkinson@gmail.com>
2016-11-24 00:49:58 -0700

    Creating helper class to more conveniently access commands in resolution
     - Part of adjudication involves finding other commands you depend upon.
       Re-scanning a list each time seems potentially expensive in the long
       term, so instead we'll cache all the commands into buckets that will
       be commonly requested.

---
+++ test/turn/test_command_map.py
@@ -0,0 +1,144 @@
+from map.predefined.vanilla_dip import generate_map
+from player.command.command import SupportCommand, MoveCommand, HoldCommand, ConvoyMoveCommand, ConvoyTransportCommand
+from player.player import Player
+from player.unit import UnitTypes
+from turn.command_map import CommandMap
+
+
+def test__move_command():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Smyrna', 'unit_type': UnitTypes.TROOP},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+    command = MoveCommand(player, player.units[0], 'Ankara')
+    command_map = CommandMap(map, [command])
+
+    assert command_map._home_map            == {'Smyrna' : command}
+    assert command_map._attacker_map        == {'Ankara' : [command]}
+    assert command_map._convoy_attacker_map == dict()
+    assert command_map._transport_map       == dict()
+    assert command_map._support_map         == dict()
+
+def test__hold_command():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Smyrna', 'unit_type': UnitTypes.TROOP},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+    command = HoldCommand(player, player.units[0])
+    command_map = CommandMap(map, [command])
+
+    assert command_map._home_map            == {'Smyrna' : command}
+    assert command_map._attacker_map        == {'Smyrna' : [command]}
+    assert command_map._convoy_attacker_map == dict()
+    assert command_map._transport_map       == dict()
+    assert command_map._support_map         == dict()
+
+def test__support_command():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Smyrna', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Armenia Coast', 'unit_type': UnitTypes.FLEET},
+        {'territory_name': 'Serbia', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Greece', 'unit_type': UnitTypes.TROOP},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+    move_command = MoveCommand(player, player.units[0], 'Ankara')
+    hold_command = HoldCommand(player, player.units[2])
+    support_command_1 = SupportCommand(player, player.units[1], player.units[0], 'Ankara')
+    support_command_2 = SupportCommand(player, player.units[3], player.units[2], 'Serbia')
+    command_map = CommandMap(map, [move_command, support_command_1, hold_command, support_command_2])
+
+    assert command_map._home_map == {
+        'Smyrna' : move_command,
+        'Armenia' : support_command_1,
+        'Serbia' : hold_command,
+        'Greece' : support_command_2,
+    }
+    assert command_map._attacker_map == {
+        'Ankara' : [move_command],
+        'Serbia' : [hold_command],
+    }
+    assert command_map._convoy_attacker_map == dict()
+    assert command_map._transport_map       == dict()
+    assert command_map._support_map == {
+        ('Smyrna', 'Ankara') : [support_command_1],
+        ('Serbia', 'Serbia') : [support_command_2],
+    }
+
+def test__convoy_move_command():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Ankara', 'unit_type': UnitTypes.TROOP},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+    command = ConvoyMoveCommand(player, player.units[0], 'Sevastopol')
+    command_map = CommandMap(map, [command])
+
+    assert command_map._home_map            == {'Ankara' : command}
+    assert command_map._attacker_map        == dict()
+    assert command_map._convoy_attacker_map == {'Sevastopol' : [command]}
+    assert command_map._transport_map       == dict()
+    assert command_map._support_map         == dict()
+
+def test__convoy_transport_command():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Ankara', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Black Sea', 'unit_type': UnitTypes.FLEET},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+    move_command = ConvoyMoveCommand(player, player.units[0], 'Sevastopol')
+    transport_command = ConvoyTransportCommand(player, player.units[1], player.units[0], 'Sevastopol')
+    command_map = CommandMap(map, [move_command, transport_command])
+
+    assert command_map._home_map            == {'Ankara' : move_command, 'Black Sea' : transport_command}
+    assert command_map._attacker_map        == dict()
+    assert command_map._convoy_attacker_map == {'Sevastopol' : [move_command]}
+    assert command_map._transport_map       == {('Ankara', 'Sevastopol') : [transport_command]}
+    assert command_map._support_map         == dict()
+
+def test__several_commands():
+    map = generate_map()
+    starting_configuration = [
+        {'territory_name': 'Ankara', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Black Sea', 'unit_type': UnitTypes.FLEET},
+        {'territory_name': 'Budapest', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Rumania Coast', 'unit_type': UnitTypes.FLEET},
+        {'territory_name': 'Ukraine', 'unit_type': UnitTypes.TROOP},
+        {'territory_name': 'Moscow', 'unit_type': UnitTypes.TROOP},
+    ]
+    player = Player("Turkey", map, starting_configuration)
+
+    commands = [
+        ConvoyMoveCommand(player, player.units[0], 'Sevastopol'),
+        ConvoyTransportCommand(player, player.units[1], player.units[0], 'Sevastopol'),
+        MoveCommand(player, player.units[2], 'Rumania'),
+        MoveCommand(player, player.units[3], 'Sevastopol Coast'),
+        SupportCommand(player, player.units[4], player.units[3], 'Sevastopol Coast'),
+        MoveCommand(player, player.units[5], 'Sevastopol'),
+    ]
+    command_map = CommandMap(map, commands)
+
+    assert command_map._home_map == {
+        'Ankara'    : commands[0],
+        'Black Sea' : commands[1],
+        'Budapest'  : commands[2],
+        'Rumania'   : commands[3],
+        'Ukraine'   : commands[4],
+        'Moscow'    : commands[5],
+    }
+    assert command_map._attacker_map == {
+        'Rumania'    : [commands[2]],
+        'Sevastopol' : [commands[3], commands[5]],
+    }
+    assert command_map._convoy_attacker_map == {
+        'Sevastopol' : [commands[0]],
+    }
+    assert command_map._transport_map == {
+        ('Ankara', 'Sevastopol') : [commands[1]],
+    }
+    assert command_map._support_map == {
+        ('Rumania', 'Sevastopol') : [commands[4]],
+    }
---
+++ turn/command_map.py
@@ -0,0 +1,75 @@
+from collections import defaultdict
+
+from map.territory import CoastTerritory
+from player.command.command import MoveCommand, ConvoyMoveCommand, ConvoyTransportCommand, SupportCommand
+
+
+class CommandMap:
+
+    """ territory -> MoveCommand[], representing commands with destination at key territory """
+    _attacker_map        = None
+    """ territory -> ConvoyMoveCommand[], representing commands with destination at key territory """
+    _convoy_attacker_map = None
+    """ (source, dest) -> ConvoyTransportCommand[], representing transport commands from source to dest territories """
+    _transport_map       = None
+    """ (source, dest) -> SupportCommand[], representing supports of units attacking from source to dest territories """
+    _support_map         = None
+    """ territory -> Command, representing command for unit originating at key territory """
+    _home_map            = None
+
+    _map = None
+
+    """ Builds CommandMap from provided Command[] """
+    def __init__(self, map, commands):
+        self._map = map
+        self._attacker_map        = defaultdict(list)
+        self._convoy_attacker_map = defaultdict(list)
+        self._transport_map       = defaultdict(list)
+        self._support_map         = defaultdict(list)
+        self._home_map            = dict()
+
+        for command in commands:
+            home_name = self._relevant_name_for_territory(command.unit.position)
+            self._home_map[home_name] = command
+            if isinstance(command, MoveCommand):
+                destination_name = self._relevant_name_for_territory(command.destination)
+                self._attacker_map[destination_name].append(command)
+            elif isinstance(command, ConvoyMoveCommand):
+                destination_name = self._relevant_name_for_territory(command.destination)
+                self._convoy_attacker_map[destination_name].append(command)
+            elif isinstance(command, ConvoyTransportCommand):
+                source = self._relevant_name_for_territory(command.transported_unit.position)
+                dest   = self._relevant_name_for_territory(command.destination)
+                self._transport_map[(source, dest)].append(command)
+            elif isinstance(command, SupportCommand):
+                source = self._relevant_name_for_territory(command.supported_unit.position)
+                dest   = self._relevant_name_for_territory(command.destination)
+                self._support_map[(source, dest)].append(command)
+
+    def _relevant_name_for_territory(self, territory_name):
+        territory = self._map.name_map[territory_name]
+        if isinstance(territory, CoastTerritory):
+            territory_name = territory.parent.name
+        return territory_name
+
+    def get_attackers(self, territory_name):
+        territory_name = self._relevant_name_for_territory(territory_name)
+        return self._attacker_map[territory_name]
+
+    def get_convoy_attackers(self, territory_name):
+        territory_name = self._relevant_name_for_territory(territory_name)
+        return self._convoy_attacker_map[territory_name]
+
+    def get_convoy_transports(self, source_name, destination_name):
+        source_name = self._relevant_name_for_territory(source_name)
+        destination_name = self._relevant_name_for_territory(destination_name)
+        return self._transport_map[(source_name, destination_name)]
+
+    def get_supports(self, source_name, destination_name):
+        source_name = self._relevant_name_for_territory(source_name)
+        destination_name = self._relevant_name_for_territory(destination_name)
+        return self._support_map[(source_name, destination_name)]
+
+    def get_home_command(self, territory_name):
+        territory_name = self._relevant_name_for_territory(territory_name)
+        return self._home_map[territory_name]
