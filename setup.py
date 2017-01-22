5ce4754cd3372f66dcb107fb6290b9a54352da6c
Aric Parkinson <aparkinson@hirevue.com>
2017-01-22 01:30:21 -0700

    One of these days...

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
+from setuptools import find_packages
 setup(
     name = 'pydip',
-    packages = ['pydip'],
-    package_data = {'pydip':['*']},
-    version = '0.1.3',
+    packages = find_packages(exclude=['pydip.test', 'pydip.test.*']),
+    version = '0.1.4',
     description = 'Adjudication logic engine for Diplomacy board game',
     author = 'Aric Parkinson',
     author_email = 'aric.parkinson@gmail.com',
     url = 'https://github.com/aparkins/pydip',
-    download_url = 'https://github.com/aparkins/pydip/tarball/0.1.3',
+    download_url = 'https://github.com/aparkins/pydip/tarball/0.1.4',
     keywords = ['diplomacy', 'adjudication', 'adjudicator', 'board', 'game'],
     classifiers = [],
 )
