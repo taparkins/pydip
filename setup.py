a8bddd6de52e8c109098deeaa88531abcfe7bb5b
Aric Parkinson <aparkinson@hirevue.com>
2017-01-22 01:12:16 -0700

    More tweaks to hopefully distribute the package library correctly

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
     name = 'pydip',
     packages = ['pydip'],
-    version = '0.1.2',
+    package_data = {'pydip':['*']},
+    version = '0.1',
     description = 'Adjudication logic engine for Diplomacy board game',
     author = 'Aric Parkinson',
     author_email = 'aric.parkinson@gmail.com',
     url = 'https://github.com/aparkins/pydip',
-    download_url = 'https://github.com/aparkins/pydip/tarball/0.1.2',
+    download_url = 'https://github.com/aparkins/pydip/tarball/0.1',
     keywords = ['diplomacy', 'adjudication', 'adjudicator', 'board', 'game'],
     classifiers = [],
 )
