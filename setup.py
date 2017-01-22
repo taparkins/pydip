baf35c491705fb725846aaf31a17f52733580498
Aric Parkinson <aparkinson@hirevue.com>
2017-01-22 00:53:43 -0700

    One of these days we'll get this upload to work. :)

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
     name = 'pydip',
-    package = ['pydip'],
-    version = '0.1',
+    packages = ['pydip'],
+    version = '0.1.2',
     description = 'Adjudication logic engine for Diplomacy board game',
     author = 'Aric Parkinson',
     author_email = 'aric.parkinson@gmail.com',
     url = 'https://github.com/aparkins/pydip',
-    download_url = 'https://github.com/aparkins/pydip/tarball/0.1',
+    download_url = 'https://github.com/aparkins/pydip/tarball/0.1.2',
     keywords = ['diplomacy', 'adjudication', 'adjudicator', 'board', 'game'],
     classifiers = [],
 )
